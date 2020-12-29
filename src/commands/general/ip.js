cf36cfd7ba97624f6489bd1663708c1cb9897ba7
NoHacksLegit <ahijadojuancruz@gmail.com>
2020-12-29 17:55:33 -0300

    Changes in IP command

--- src/commands/general/ip.js
+++ src/commands/general/ip.js
@@ -30,32 +30,33 @@ module.exports = class extends Command {
     }
 
     async init() {
-        const ping = require("minecraft-server-util");
-        const request = require("request")
-        let pingURL = `https://api.minetools.eu/ping/mc.minergame.ml`; // Es la herramienta de donde se sacara una parte de la informacion del servidor
-        let args = "mc.minergame.ml"
-        let port = "25565"
-        request(pingURL, function(err, resp, body) {
-          if (err) return console.log(err.message);
+      const ping = require('minecraft-server-util') // Instala el NPM antes
+      const request = require("request");     // Instala el NPM Antes
+       var port = args[1] 
+      let ip = "mc.minergame.ml"
+      if(!port) {
+        port = `25565` 
+      } // Si no ponen un puerto, el predeterminado será 25565
+          let pingURL = `https://api.minetools.eu/ping/${ip}`
+        request(pingURL, function(err, resp, body){
+          if(err) return console.log(err.message);
           body = JSON.parse(body);
-          if (body.error)
-            return message.channel.send(":x: Servidor fuera de linea o no disponible. :x:");
-          let motd = `http://status.mclive.eu/MinecraftServer/mc.minergame.ml/25565/banner.png`;
-          ping(args, parseInt(port), (error, reponse) => {
-            const Embed = new Discord.MessageEmbed()
-              .setTitle(`Status del servidor`)
-              .addField("IP", reponse.host)
-              .addField("Version", reponse.version)
-              .addField("Ping/Latencia", body.latency)
-              .addField("Jugadores Online", reponse.onlinePlayers + "/" + reponse.maxPlayers
-              )
-              .setImage(motd)
-              .setThumbnail("https://cdn.discordapp.com/icons/643543733710880813/a_69e6bf1a957393137f07ade4b2b92328.gif"
-              ); // Totalmente opcional.
-    
-            message.channel.send(Embed); //Envia el embed con la info del servidor
-          });
-        });
-
+          if(body.error) return message.channel.send(":x: `Error | Servidor fuera de linea o no disponible.`") // Si la herramienta no encuentra nada sobre la ip
+           let motd = `http://status.mclive.eu/MinecraftServer/${ip}/25565/banner.png` // Imagen del motd del servidor.
+                  ping(ip, parseInt(port), (error, reponse) =>{ // Usando minecraft-server-until para sacar informacion mas especifica
+                      if(error) return message.channel.send('No puedo encontrar ese servidor'); // Por si minetools lo reconoce pero la otra api no.
+                      const Embed = new Discord.MessageEmbed()
+                      .setTitle('Server Status')
+                      .addField('Server IP', reponse.host) // ip del servidor
+                      .addField('Server Version', reponse.version) // version del servidor
+                      .addField('Latency', body.latency) // latencia del servidor con pingURL y request
+                      .addField('Online Players', reponse.onlinePlayers + "/" + reponse.maxPlayers) // Jugadores online y limite de jugadores
+                      .setImage(motd) // Motd antes definido
+                      .setThumbnail('https://cdn.glitch.com/402b9099-0636-457a-8ffb-faf65c857490%2F1.png?v=1585792839856') // Totalmente opcional.
+                      message.channel.send(Embed) //Envia el embed con la info del servidor
+                      } )
+        
+                       
+              })
 };
 }
